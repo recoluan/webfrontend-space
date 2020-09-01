6316396fa4ec06dc9ee9606e9a27f426dc63ab05
chenchen0224 <1293485100@qq.com>
2020-09-02 00:10:05 +0800

    update RAF

---
+++ demos/Part07-页面渲染与性能优化/RAF/RAF.js
@@ -0,0 +1,27 @@
+// 需求：3s 把宽度从 100px 变为 640px ，即增加 540px
+// 60帧/s ，3s 180 帧 ，每帧变化 3px
+
+
+const $div1 = $('#div1')
+let curWidth = 100
+const maxWidth = 640
+
+// 1. setTimeout
+// function animate() {
+//     curWidth = curWidth + 3
+//     $div1.css('width', curWidth)
+//     if (curWidth < maxWidth) {
+//         setTimeout(animate, 16.7) // 自己控制时间
+//     }
+// }
+// animate()
+
+// 2. RAF
+function animate() {
+    curWidth = curWidth + 3
+    $div1.css('width', curWidth)
+    if (curWidth < maxWidth) {
+        window.requestAnimationFrame(animate) // 时间不用自己控制
+    }
+}
+animate()
